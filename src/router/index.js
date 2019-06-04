c904048fd67da41a25996f5ee2487e65f8455093
Adilson Sousa <adilsonsdesousa@gmail.com>
2019-06-03 21:35:23 -0300

    Criando rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Home from '@/pages/Home'
+import Dashboard from '@/pages/Dashboard'
+import Config from '@/pages/Config'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/config',
+      name: 'config',
+      component: Config
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: Home
     }
-  ]
+  ],
+  mode: 'history'
 })
