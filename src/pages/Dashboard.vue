f36aa381c89cc1105caa12babf2f2e173b3958ad
Adilson Sousa <adilsonsdesousa@gmail.com>
2019-06-15 19:37:17 -0300

    Instalação e teste Vue-ChartJS

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,3 +1,47 @@
 <template>
-    <h1>Dashboard works!</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Dashoboard works!</h1>
+        <grafico :width="1110" :height="400" :chart-data="datacollection"></grafico>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+import Grafico from "@/components/Grafico.vue";
+import { setTimeout } from 'timers';
+export default {
+  components: {
+    Grafico
+  },
+  data() {
+    return {
+      datacollection: null
+    };
+  },
+  mounted() {
+    setTimeout(
+    this.carregarDadosDoGrafico(), 1000);
+  },
+  methods: {
+    carregarDadosDoGrafico() {
+      const dias = ['01/06', '02/06', '03/06', '10/06', '11/06', '13/06'];
+      const tempo = 
+
+      this.datacollection = {
+        labels: dias,
+        datasets: [
+          {
+            label: "Tempo",
+            backgroundColor: "transparent",
+            borderColor: "green",
+            data: [0, 5, 2, 9, 1, 4]
+          }
+        ]
+      };
+    }
+  }
+};
+</script>
+
